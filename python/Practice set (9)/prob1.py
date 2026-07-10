4727575ac834f3146878ea20f96582d376f7a421
vivekmittal110 <vivekmittal110230@gmail.com>
2026-07-10 12:30:36 +0530

    Completed Prob1

---
+++ python/Practice set (9)/prob1.py
@@ -0,0 +1,20 @@
+# # # f = open("file.txt")
+# # f = open("file.txt")
+
+# # line = f.readline()
+# # while(line != ""):
+# #     print(line)
+
+# #     line = f.readline()
+# # # print(line)
+# # f.close()
+
+
+
+
+
+# with open("file.txt") as f:
+#     line = f.readline()
+#     while(line != ""):
+#         print(line)
+#         line = f.readline()
