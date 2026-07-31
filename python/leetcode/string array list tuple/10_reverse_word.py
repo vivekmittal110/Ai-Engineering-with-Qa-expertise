72fe8009a53939c7a168b5c538a7dbc52faeb8a4
vivekmittal110 <vivekmittal110230@gmail.com>
2026-07-31 12:42:54 +0530

    completed strings ds

--- python/leetcode/string array list tuple/10_reverse_word.py
+++ python/leetcode/string array list tuple/10_reverse_word.py
@@ -1,4 +1,5 @@
 s = "the sky    is blue   "
 m = s.split()
+print(m)
 new = " ".join(m[::-1])
 print(new)
---
+++ python/leetcode/string array list tuple/11_lenght_of_last_word.py
@@ -0,0 +1,5 @@
+s = "   fly me   to   the moonnn     "
+# l= s.strip()
+l = s.split()
+print(l)
+print(len(l[-1]))
