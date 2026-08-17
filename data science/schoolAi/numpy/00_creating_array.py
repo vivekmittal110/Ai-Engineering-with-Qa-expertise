f7d2888e9f9de79eed97dc596d94b69d404fb793
vivekmittal110 <vivekmittal110230@gmail.com>
2026-08-17 20:29:23 +0530

    Completed 00 Creating Array

---
+++ data science/schoolAi/numpy/00_creating_array.py
@@ -0,0 +1,15 @@
+import numpy as np
+arr = np.array([1,2,3,4,5])
+print(arr)
+
+zeros = np.zeros((4,4))
+print(zeros)
+
+ones = np.ones((2,2))
+print(ones)
+
+range_arr = np.arange(2,11 ,2)
+print(range_arr)
+
+linspace_arr = np.linspace(0,10,5)
+print(linspace_arr)
