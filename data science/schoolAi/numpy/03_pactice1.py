24f739ca5a6f7bfb9a96c3a4c884d2c4380140a5
vivekmittal110 <vivekmittal110230@gmail.com>
2026-08-19 05:08:42 +0530

    Completed 03 Pactice1

---
+++ data science/schoolAi/numpy/03_pactice1.py
@@ -0,0 +1,10 @@
+import numpy as np
+arr1 = np.arange(1,6)
+arr2 = np.arange(6,11)
+
+print(arr1 + arr2)
+print(arr1 - arr2)
+print(arr1 * arr2)
+print(arr1 / arr2)
+print(arr1 // arr2)
+print(arr1 % arr2)
