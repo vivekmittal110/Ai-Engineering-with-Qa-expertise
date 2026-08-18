87ebda8059ae24e74dbc8b2dd2fbeb0873a8c962
vivekmittal110 <vivekmittal110230@gmail.com>
2026-08-18 17:48:06 +0530

    Completed 02 Operations Arr

---
+++ data science/schoolAi/numpy/02_operations_arr.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+a = np.array([1,2,3])
+b = np.array([4,5,6])
+
+# print(a + b)
+# print(a - b)
+# print(a * b)
+# print(a / b)
+# print(a ** 2) #square
+
+
+# mathmatical operation
+s = np.array([1,4,9,16,25])
+# print(np.sqrt(s))
+# print(np.sum(s))
+# print(np.mean(s))
+# print(np.min(s))
+# print(np.max(s))
+
+
+
